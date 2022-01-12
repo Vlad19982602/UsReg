9ee8395b8bf39e4b52f721d3aa20eec7a2164e7c
Vlad19982602 <79852880+Vlad19982602@users.noreply.github.com>
2022-01-12 12:31:37 +0500

    Add project files.

---
+++ UsReg/AuthWindow.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Media.Animation;
+
+namespace UsReg
+{
+    /// <summary>
+    /// Interaction logic for AuthWindow.xaml
+    /// </summary>
+    public partial class AuthWindow : Window
+    {
+        public AuthWindow()
+        {
+            InitializeComponent();
+
+            DoubleAnimation doubleAnimation = new DoubleAnimation();
+            doubleAnimation.From = 0;
+            doubleAnimation.To = 450;
+            doubleAnimation.Duration = TimeSpan.FromSeconds(3);
+            regButton.BeginAnimation(Button.WidthProperty, doubleAnimation);
+        }
+
+        private void Button_Auth_Click(object sender, RoutedEventArgs e)
+        {
+            string login = textBoxLogin.Text.Trim();
+            string pass1 = PasssBox1.Password.Trim();
+
+            if (login.Length < 5)
+            {
+                textBoxLogin.ToolTip = "Поле введено некорректно";
+                textBoxLogin.Foreground = Brushes.Red;
+            }
+            else if (pass1.Length < 5)
+            {
+                PasssBox1.ToolTip = "Пароли не совпадают";
+                PasssBox1.Foreground = Brushes.Red;
+            }
+            else
+            {
+                textBoxLogin.ToolTip = "";
+                textBoxLogin.Background = Brushes.Transparent;
+                PasssBox1.ToolTip = "";
+                PasssBox1.Background = Brushes.Transparent;
+
+                User authUser = null;
+                using(ApplicationContext db = new ApplicationContext())
+                {
+                    authUser = db.Users.Where(b => b.Login == login && b.Pass == pass1).FirstOrDefault();
+                }
+
+                if (authUser != null)
+                {
+                    MessageBox.Show("Все хорошо");
+                    UserPageWindow userPageWindow = new UserPageWindow();
+                    userPageWindow.Show();
+                    Hide();
+                }
+                else
+                    MessageBox.Show("Вы ввели что-то некорректно");
+            }
+        }
+
+        private void Button_Reg_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            Hide();
+        }
+    }
+}
---
+++ UsReg/MainWindow.xaml.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Media.Animation;
+
+namespace UsReg
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ApplicationContext db;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            db = new ApplicationContext();
+
+            DoubleAnimation doubleAnimation = new DoubleAnimation();
+            doubleAnimation.From = 0;
+            doubleAnimation.To = 450;
+            doubleAnimation.Duration = TimeSpan.FromSeconds(3);
+            regButton.BeginAnimation(Button.WidthProperty, doubleAnimation);
+
+        }
+
+        private void Button_Reg_Click(object sender, RoutedEventArgs e)
+        {
+            string login = textBoxLogin.Text.Trim();
+            string pass1 = PasssBox1.Password.Trim();
+            string pass2 = PassBox2.Password.Trim();
+            string email = textBoxEmail.Text.Trim().ToLower();
+
+            if(login.Length < 5)
+            {
+                textBoxLogin.ToolTip = "Поле введено некорректно";
+                textBoxLogin.Foreground = Brushes.Red;
+            }
+            else if(pass1.Length < 5)
+            {
+                PasssBox1.ToolTip = "Пароли не совпадают";
+                PasssBox1.Foreground = Brushes.Red;
+            }
+            else if(pass2 != pass1)
+            {
+                PassBox2.ToolTip = "Пароли не совпадают";
+                PassBox2.Foreground = Brushes.Red;
+            }
+            else if(email.Length < 5 || !email.Contains("@") || !email.Contains("."))
+            {
+                textBoxEmail.ToolTip = "Не правильное написание. Возможно упущена точка(.) или собачка(@)";
+                textBoxEmail.Background = Brushes.Red;
+            }
+            else
+            {
+                textBoxLogin.ToolTip = "";
+                textBoxLogin.Background = Brushes.Transparent;
+                PasssBox1.ToolTip = "";
+                PasssBox1.Background = Brushes.Transparent;
+                PassBox2.ToolTip = "";
+                PassBox2.Background = Brushes.Transparent;
+                textBoxEmail.ToolTip = "";
+                textBoxEmail.Background = Brushes.Transparent;
+
+                User user = new User(login, email, pass1);
+
+                db.Users.Add(user);
+                db.SaveChanges();
+
+                AuthWindow authWindow = new AuthWindow();
+                authWindow.Show();
+                Hide();
+            }
+
+            
+        }
+
+        private void Button_Window_Auth_Click(object sender, RoutedEventArgs e)
+        {
+            AuthWindow authWindow = new AuthWindow();
+            authWindow.Show();
+            Hide();
+        }
+    }
+}
